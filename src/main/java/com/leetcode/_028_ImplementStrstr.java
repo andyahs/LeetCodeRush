24ac1c23dbfec34a752e1667a1f64eddf9008aa4
andyahs <andyhas@163.com>
2018-08-24 10:25:52 +0800

    更新028

--- src/main/java/com/leetcode/_028_ImplementStrstr.java
+++ src/main/java/com/leetcode/_028_ImplementStrstr.java
@@ -1,25 +1,31 @@
 package com.leetcode;
 
 public class _028_ImplementStrstr {
-    public static  int Strstr(String aString ,String needle){
-        if (needle.isEmpty() ||aString.length() <needle.length()){
+    public static  int Strstr(String haystack ,String needle){
+        if (needle.length()==0) return 0;
+        if (needle.length() ==haystack.length() && needle ==haystack) return 0;
+        if (haystack.length() <needle.length()){
             return -1;
         }
-        for(int strCount= 0; strCount<=aString.length() - needle.length();strCount++)
-            if (aString.charAt(strCount) == needle.charAt(0)) {
+        for(int strCount= 0; strCount<=haystack.length() - needle.length();strCount++)
+            if (haystack.charAt(strCount) == needle.charAt(0)) {
+                if (needle.length()==1) return strCount;
                 for (int subCount = 1; subCount < needle.length(); subCount++) {
-                    if(aString.charAt(strCount+subCount) != needle.charAt(subCount)){
+                    if(haystack.charAt(strCount+subCount) != needle.charAt(subCount)){
                         break;
                     }
+                    if (subCount ==needle.length()-1) return strCount;
                 }
-                return strCount;
+
             }
         return -1;
+        //leetcode某个解法 让我吐血
+        //return haystack.indexOf(needle);
     }
 
     public static void main(String[] args) {
-        String aString  = "abc123456789";
-        String needle = "789";
-        System.out.println("index:"+Strstr(aString,needle));
+        String haystack  = "aaaa";
+        String needle = "baaa";
+        System.out.println("index:"+Strstr(haystack,needle));
     }
 }
