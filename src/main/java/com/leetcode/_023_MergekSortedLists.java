f230214b677e7e5891356db3c194844b40c132db
andyahs <andyhas@163.com>
2018-08-27 00:04:49 +0800

    更新23

--- src/main/java/com/leetcode/_023_MergekSortedLists.java
+++ src/main/java/com/leetcode/_023_MergekSortedLists.java
@@ -1,25 +1,27 @@
 package com.leetcode;
 
-import java.awt.List;
-//�ϲ������������
 public class _023_MergekSortedLists<AnyType> {
+//合并两个链表，直接用迭代法实现
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-	public  MyList<AnyType>[] mergeKLst ( MyList<AnyType>[] lists ){
-		
-		if(lists.length ==0){
-			return null;
+		if (l1 == null) return l2;
+		if (l2 == null) return l1;
+		if (l1.val < l2.val) {
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
 		}
-		int cntLen = lists.length;
-		while(cntLen >1){
-			int k = cntLen/2;
-			for(int i=0; i<k;i++){
-				MyList<AnyType> mg2Lst = new MyList<AnyType>();
-				lists[i] = mg2Lst.merge2Lists(lists[i], lists[i+k]);
-			}
-			cntLen = k;
-		}
-		
-		return lists;
-		
+	}
+
+	private static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+
+	public static void main(String[] args) {
+
 	}
 }
