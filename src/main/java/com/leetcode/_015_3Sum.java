41826fd73040de8ac6a3cc0dd2095a286db57369
andyahs <andyhas@163.com>
2018-09-06 16:00:30 +0800

    更新015三数之和

--- src/main/java/com/leetcode/_015_3Sum.java
+++ src/main/java/com/leetcode/_015_3Sum.java
@@ -1,32 +1,49 @@
 package com.leetcode;
 
+
+
+import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 //a+b+c = 0
 //without sort, print the same answer
+//为了将O(n^3)压缩到O(n^2)，用了map，但是依然在超时的边缘试探。。。
 public class _015_3Sum {
-	
-	public static void find3Sum(int [] a){
+
+	public static List<List<Integer>> threeSum(int[] nums) {
 		int target = 0;
-		Arrays.sort(a);
-		System.out.println(Arrays.toString(a));
+		Arrays.sort(nums);
+		List<List<Integer>> res =new ArrayList<>();
+
 		HashMap<Integer, Integer> x = new HashMap<Integer, Integer>();
-		for(int i=0;i<a.length;i++){
-			x.put(a[i], i);
+		for(int i=0;i<nums.length;i++){
+			x.put(nums[i], i);
 		}
-		for(int i = 0; i<a.length;i++){
-			//if(x.get(target - a[i]) != null){
-				for(int j = i+1 ; j<a.length-1 ; j++){
-					if(x.get(target-a[i]-a[j])!= null){
-						System.out.println(i+" "+a[i]+" "+j+" "+a[j]+" "+x.get(target-a[i]-a[j])+((target-a[i]-a[j])));
+		for(int i = 0; i<nums.length&& nums[i]<=0;i++){
+			for(int j = i+1 ; j<nums.length-1 ; j++){
+				if(x.get(target-nums[i]-nums[j])!= null && x.get(target-nums[i]-nums[j])>j ){
+					List<Integer> curData = new ArrayList<>();
+					curData.add(nums[i]);
+					curData.add(nums[j]);
+					curData.add(target - nums[i] - nums[j]);
+					res.add(curData);
+					while (j+1<nums.length && nums[j]==nums[j+1]){
+						j++;
 					}
-			//	}
+				}
 			}
+			while (i+1<nums.length && nums[i]==nums[i+1]) i++;
 		}
+		return res;
 	}
 	public static void main(String[] args) {
-		int a[]={2,5,8,-6,9,7,-2};
-		find3Sum(a);
+//		int a[]={2,5,8,-6,9,7,-2};
+//		int a[]={-1,0,1,2,-1,-4};
+//		int a[]={0,0,0,0};
+		int a[]={-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6}; //6
+		threeSum(a);
 	}
 }
