6fe6ba5b815fb7a39336d875b7a0a3b23b6b027b
andyahs <andyhas@163.com>
2018-09-07 15:00:00 +0800

    更新003

--- src/main/java/com/leetcode/_003_LongestSubStr.java
+++ src/main/java/com/leetcode/_003_LongestSubStr.java
@@ -2,24 +2,26 @@ package com.leetcode;
 
 import java.util.Arrays;
 
-//����һ���ַ����в��ظ�������ַ���
+//无重复字符的最长子串的长度
 public class _003_LongestSubStr {
 
 	public static int lengthOfLongestSubStr(String s){
+		//这个奇葩方法就是用字符转码做下标，然后存储最后一个出现的位置
+		//i-left得出目前字符串长度
 		int[] m = new int [256];
 		Arrays.fill(m,-1);
 		int res = 0,left = -1;
 		for(int i = 0 ;i<s.length();i++){
 			left = Math.max(left, m[s.charAt(i)]);
-			System.out.println("left:"+left);
-			System.out.println("s.charAt(i):"+s.charAt(i));
+//			System.out.println("left:"+left);
+//			System.out.println("s.charAt(i):"+s.charAt(i));
 			m[s.charAt(i)] = i;
-			System.out.println("m[s.charAt(i)]:"+m[s.charAt(i)]);
+//			System.out.println("m[s.charAt(i)]:"+m[s.charAt(i)]);
 			res = Math.max(res, i - left);
-			System.out.println("res:"+res);
-			System.out.println("-----------------");
+//			System.out.println("res:"+res);
+//			System.out.println("-----------------");
 		}
-		System.out.println("m[c]:"+m['c']+"mc_int"+(int)('a'));
+
 		return res;
 	}
 	public static void main(String[] args) {
