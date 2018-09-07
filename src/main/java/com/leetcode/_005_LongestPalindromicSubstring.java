30a9d2cea46ef6a28073d459fb4bc2f724f0f8da
andyahs <andyhas@163.com>
2018-09-07 15:29:20 +0800

    更新005

--- src/main/java/com/leetcode/_005_LongestPalindromicSubstring.java
+++ src/main/java/com/leetcode/_005_LongestPalindromicSubstring.java
@@ -37,6 +37,37 @@ public class _005_LongestPalindromicSubstring {
 		}
 		System.out.println("resId:"+resId+"  resMx:"+resMx);
 		return s.substring((resId-resMx)/2,(resId+resMx)/2-1);
+		//以下是一个算法
+		//    public String longestPalindrome(String s) {
+        //       if(s.length()<=1) return  s;
+        //        char[] characters = s.toCharArray();
+        //        //用于存放str中最长回文子串所对应的下标
+        //        int[] range = {0,1};
+        //        for(int i = 0;i<characters.length;i++){
+        //            i = helper(i,characters,range);
+        //        }
+        //        return s.substring(range[0],range[1]);
+        //    }
+        //
+        //    private int helper(int index,char[] c,int[] range) {
+        //        int low = index;
+        //        int high = index;
+        //        //如果遇到相同字符，则high进位，如abba ，这样偶回文子串也可以当做奇回文处理了
+        //        while(high<c.length-1 && c[high]==c[high+1]){
+        //            high++;
+        //        }
+        //        int cursor = high;
+        //
+        //        while(high+1<c.length&&low-1>=0&&c[low-1]==c[high+1]){
+        //            low--;
+        //            high++;
+        //        }
+        //        if(high-low+1>range[1]-range[0]){
+        //            range[0] = low;
+        //            range[1] = high + 1;
+        //        }
+        //        return cursor;
+        //    }
 	}
 	public static void main(String[] args) {
 //		String s = "abatggfs54765789987rty";
