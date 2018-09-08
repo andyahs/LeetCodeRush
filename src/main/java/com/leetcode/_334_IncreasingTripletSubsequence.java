81dfac5a24a4cd1c1b5cf68e9f5d62fb566e8746
andyahs <andyhas@163.com>
2018-09-08 17:16:23 +0800

    更新334三元递增子序列

---
+++ src/main/java/com/leetcode/_334_IncreasingTripletSubsequence.java
@@ -0,0 +1,53 @@
+package com.leetcode;
+
+
+/**
+ * Created by Andiz
+ * 2018/9/8 15:56
+ * coding For love and Peace
+ * 递增的三元子序列
+ * 给定一个未排序的数组，判断这个数组中是否存在长度为 3 的递增子序列。
+ *
+ * 数学表达式如下:
+ *
+ *     如果存在这样的 i, j, k,  且满足 0 ≤ i < j < k ≤ n-1，
+ *     使得 arr[i] < arr[j] < arr[k] ，返回 true ; 否则返回 false 。
+ *
+ * 说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1) 。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,4,5]
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入: [5,4,3,2,1]
+ * 输出: false
+ */
+public class _334_IncreasingTripletSubsequence {
+    public static boolean increasingTriplet(int[] nums) {
+        if (null==nums ||nums.length<=2) return false;
+        int min=Integer.MAX_VALUE,mid = Integer.MAX_VALUE;
+
+        for (int i=0;i<nums.length;i++) {
+            int curVal = nums[i];
+            if (curVal<min){
+                min = curVal;
+            }else if(min<curVal &&curVal <mid){
+                mid =curVal;
+            }else if(curVal!=min &&curVal!=mid) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        int [] nums ={5,4,3,2,1};
+        //[2,4,-2,-3]
+//        int [] nums ={2,4,-2,-3};
+        int [] nums ={1,2,3,4,5};
+        System.out.println(increasingTriplet(nums));
+    }
+}
