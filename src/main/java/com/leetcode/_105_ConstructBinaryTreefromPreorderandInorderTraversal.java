cded9455a4a3fb52bfa0530ebdca9ec9f7c1d38d
andyahs <andyhas@163.com>
2018-09-12 14:25:54 +0800

    更新105

--- src/main/java/com/leetcode/_105_ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/main/java/com/leetcode/_105_ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -6,6 +6,9 @@ package com.leetcode;
  * coding For love and Peace
  * 从前序与中序遍历序列构造二叉树
  *
+ * Preorder:　  　5　　4　　11　　8　　13　　9
+ * Inorder:　　 　11　　4　　5　　13　　8　　9
+ *
  */
 public class _105_ConstructBinaryTreefromPreorderandInorderTraversal {
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -19,8 +22,8 @@ public class _105_ConstructBinaryTreefromPreorderandInorderTraversal {
             if (preorder[pLeft] == inorder[i]) break;
         }
         TreeNode cur = new TreeNode(preorder[pLeft]);
-        cur.left = buildTree(preorder, pLeft + 1, pLeft + i - iLeft, inorder, iLeft, i - 1);
-        cur.right = buildTree(preorder, pLeft + i - iLeft + 1, pRight, inorder, i + 1, iRight);
+        cur.left = buildTree(preorder, pLeft + 1, pLeft + i - iLeft, inorder, iLeft, i - 1);//找到左子树范围
+        cur.right = buildTree(preorder, pLeft + i - iLeft + 1, pRight, inorder, i + 1, iRight);//找到右子树范围
         return cur;
     }
 
