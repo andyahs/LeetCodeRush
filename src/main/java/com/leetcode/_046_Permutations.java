dd061fb6ac075cb3450abc1ae3cd40f361284186
andyahs <andyhas@163.com>
2018-09-18 00:03:11 +0800

    更新046

--- src/main/java/com/leetcode/_046_Permutations.java
+++ src/main/java/com/leetcode/_046_Permutations.java
@@ -12,58 +12,58 @@ import java.util.List;
  */
 public class _046_Permutations {
 
-//    public static List<List<Integer>> permute(int[] nums) {
-//        List<List<Integer>> res = new LinkedList<>();
-//        if (null ==nums ||nums.length <1) return res;
-//        boolean[] flags = new boolean[nums.length];
-//
-//        List<Integer> curList = new LinkedList<>();
-//        permute(nums,flags,curList,res);
-//        return res;
-//    }
-//    public  void permute(int[] nums,boolean[] flags, List<Integer>curList,List<List<Integer>> res){
-//        if (curList.size() ==nums.length) {
-//            res.add(curList);
-//            return;
-//        }
-//        for (int i = 0;i<nums.length ; i++){
-//            if (!flags[i]) {
-//                curList.add(nums[i]);
-//                flags[i] =true;
-//                permute(nums,flags,curList,res);
-//                curList.remove(curList.size()-1);
-//                flags[i] = false;
-//            }
-//        }
-//
-//    }
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (null ==nums ||nums.length <1) return res;
+        boolean[] flags = new boolean[nums.length];
 
-    public static List<List<Integer>> permute(int[] num) {
-        List<List<Integer>> res=new ArrayList<>();
-        int len=num.length;
-        if(num==null||len==0){
-            return res;
-        }
-        boolean [] visit=new boolean [len];
-        ArrayList<Integer> list=new ArrayList<>();
-        dfs(num,list,visit,res);
+        List<Integer> curList = new LinkedList<>();
+        permute(nums,flags,curList,res);
         return res;
     }
-    public static void dfs(int [] num,ArrayList<Integer> list,boolean [] visit,List<List<Integer>> res){
-        if(list.size()==num.length){
-            res.add(new ArrayList<Integer>(list));
-            return ;
+    public  static void permute(int[] nums,boolean[] flags, List<Integer>curList,List<List<Integer>> res){
+        if (curList.size() ==nums.length) {
+            res.add(new LinkedList<>(curList));
+            return;
         }
-        for(int i=0;i<num.length;i++){
-            if(!visit[i]){
-                visit[i]=true;
-                list.add(num[i]);
-                dfs(num,list,visit,res);
-                list.remove(list.size()-1);
-                visit[i]=false;
+        for (int i = 0;i<nums.length ; i++){
+            if (!flags[i]) {
+                curList.add(nums[i]);
+                flags[i] =true;
+                permute(nums,flags,curList,res);
+                curList.remove(curList.size()-1);
+                flags[i] = false;
             }
         }
+
     }
+
+//    public static List<List<Integer>> permute(int[] num) {
+//        List<List<Integer>> res=new ArrayList<>();
+//        int len=num.length;
+//        if(num==null||len==0){
+//            return res;
+//        }
+//        boolean [] visit=new boolean [len];
+//        ArrayList<Integer> list=new ArrayList<>();
+//        dfs(num,list,visit,res);
+//        return res;
+//    }
+//    public static void dfs(int [] num,ArrayList<Integer> list,boolean [] visit,List<List<Integer>> res){
+//        if(list.size()==num.length){
+//            res.add(new ArrayList<Integer>(list));
+//            return ;
+//        }
+//        for(int i=0;i<num.length;i++){
+//            if(!visit[i]){
+//                visit[i]=true;
+//                list.add(num[i]);
+//                dfs(num,list,visit,res);
+//                list.remove(list.size()-1);
+//                visit[i]=false;
+//            }
+//        }
+//    }
 
     public static void main(String[] args) {
         int[] nums= {1,2,3};
