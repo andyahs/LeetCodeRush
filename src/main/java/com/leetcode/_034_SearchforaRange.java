a167f2f2de7474a021f5e1a90aa8fdccdcd00cd3
andyahs <andyhas@163.com>
2018-09-24 23:31:40 +0800

    更新034在排序数组中查找元素的第一个和最后一个位置

---
+++ src/main/java/com/leetcode/_034_SearchforaRange.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+/**
+ * Created by Andiz
+ * 2018/9/24 23:25
+ * coding For love and Peace
+ * 034
+ * 在排序数组中查找元素的第一个和最后一个位置
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 如果数组中不存在目标值，返回 [-1, -1]。
+ * 示例 1:
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: [3,4]
+ * 示例 2:
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: [-1,-1]
+ */
+public class _034_SearchforaRange {
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] res = new int[2];
+        res[0] = -1;
+        res[1] = -1;
+        if (null == nums || nums.length ==0) return res;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        if (nums[right] != target) return res;
+        res[0] = right;
+        right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) left = mid + 1;
+            else right= mid;
+        }
+        res[1] = left - 1;
+        return res;
+
+    }
+}
