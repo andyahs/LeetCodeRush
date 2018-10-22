6c5d86bade4ba911de55469278b3f27fd3d7afe1
andyahs <andyhas@163.com>
2018-10-22 23:22:46 +0800

    更新105从前序与中序遍历序列构造二叉树

--- src/main/java/com/leetcode/_105_ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/main/java/com/leetcode/_105_ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -4,7 +4,7 @@ package com.leetcode;
  * Created by Andiz
  * 2018/9/12 11:07
  * coding For love and Peace
- * 从前序与中序遍历序列构造二叉树
+ * 105从前序与中序遍历序列构造二叉树
  *
  * Preorder:　  　5　　4　　11　　8　　13　　9
  * Inorder:　　 　11　　4　　5　　13　　8　　9
@@ -27,6 +27,35 @@ public class _105_ConstructBinaryTreefromPreorderandInorderTraversal {
         return cur;
     }
 
+    //LeetCode提供
+    //    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    //        if (preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0) {
+    //            return null;
+    //        }
+    //        return build(preorder, inorder, 0, 0, inorder.length - 1);
+    //    }
+    //
+    //    public TreeNode build(int[] preOrder, int[] inOrder, int preIndex, int startMidIndex, int endMidIndex) {
+    //        if (preIndex > preOrder.length - 1 || startMidIndex > endMidIndex) {
+    //            return null;
+    //        }
+    //        TreeNode node = new TreeNode(preOrder[preIndex]);
+    //        int rootMidIndex = endMidIndex;
+    //        while (rootMidIndex > startMidIndex) {
+    //            if (node.val == inOrder[rootMidIndex]) {
+    //                break;
+    //            }
+    //            rootMidIndex--;
+    //        }
+    //        if (rootMidIndex > startMidIndex) {
+    //            node.left = build(preOrder, inOrder, preIndex + 1, startMidIndex, rootMidIndex - 1);
+    //        }
+    //        if (rootMidIndex < endMidIndex) {
+    //            node.right = build(preOrder, inOrder, preIndex + (rootMidIndex - startMidIndex) + 1,
+    //                    rootMidIndex + 1, endMidIndex);
+    //        }
+    //        return node;
+    //    }
     public static class TreeNode {
         int val;
         TreeNode left;
