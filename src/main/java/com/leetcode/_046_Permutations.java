cc8d5a3a50e9ff55f9419e6b28e2b6dddc7491c3
andyahs <andyhas@163.com>
2018-10-27 23:54:36 +0800

    更新046全排列

--- src/main/java/com/leetcode/_046_Permutations.java
+++ src/main/java/com/leetcode/_046_Permutations.java
@@ -9,6 +9,19 @@ import java.util.List;
  * 2018/9/17 0:19
  * coding For love and Peace
  * 046全排列
+ *给定一个没有重复数字的序列，返回其所有可能的全排列。
+ *
+ * 示例:
+ * 输入: [1,2,3]
+ * 输出:
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1],
+ *   [3,1,2],
+ *   [3,2,1]
+ * ]
  */
 public class _046_Permutations {
 
@@ -64,6 +77,31 @@ public class _046_Permutations {
 //            }
 //        }
 //    }
+
+    // LeetCode提供
+    //    public List<List<Integer>> permute(int[] nums) {
+    //        List<List<Integer>> res = new ArrayList<List<Integer>>();
+    //        ArrayList<Integer> temp = new ArrayList<Integer>();
+    //        dfs(res, nums, 0);
+    //        return res;
+    //    }
+    //    private void dfs(List<List<Integer>> res, int[] nums, int j) {
+    //        if (j == nums.length) {
+    //            List<Integer> temp = new ArrayList<Integer>();
+    //            for (int num : nums) temp.add(num);
+    //            res.add(temp);
+    //        }
+    //        for (int i = j; i < nums.length; i++) {
+    //            swap(nums, i, j);
+    //            dfs(res, nums, j+1);
+    //            swap(nums, i, j);
+    //        }
+    //    }
+    //    private void swap(int[] nums, int m, int n) {
+    //        int temp = nums[m];
+    //        nums[m] = nums[n];
+    //        nums[n] = temp;
+    //    }
 
     public static void main(String[] args) {
         int[] nums= {1,2,3};
