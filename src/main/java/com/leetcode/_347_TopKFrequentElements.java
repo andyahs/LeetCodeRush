c62c8e365922b9ed4edc8e3d96f33770fc94e931
andyahs <andyhas@163.com>
2018-10-31 11:16:23 +0800

    更新347前K个高频元素

--- src/main/java/com/leetcode/_347_TopKFrequentElements.java
+++ src/main/java/com/leetcode/_347_TopKFrequentElements.java
@@ -31,93 +31,101 @@ public class _347_TopKFrequentElements {
         }
     }
 
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        //count the frequency for each element
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int num: nums){
-            if(map.containsKey(num)){
-                map.put(num, map.get(num)+1);
-            }else{
-                map.put(num, 1);
+//    public List<Integer> topKFrequent(int[] nums, int k) {
+//        //count the frequency for each element
+//        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+//        for(int num: nums){
+//            if(map.containsKey(num)){
+//                map.put(num, map.get(num)+1);
+//            }else{
+//                map.put(num, 1);
+//            }
+//        }
+//
+//        // create a min heap
+//        PriorityQueue<Pair> queue = new PriorityQueue<Pair>(new Comparator<Pair>(){
+//            public int compare(Pair a, Pair b){
+//                return a.count-b.count;
+//            }
+//        });
+//
+//        //maintain a heap of size k.
+//        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+//            Pair p = new Pair(entry.getKey(), entry.getValue());
+//            queue.offer(p);
+//            if(queue.size()>k){
+//                queue.poll();
+//            }
+//        }
+//
+//        //get all elements from the heap
+//        List<Integer> result = new ArrayList<Integer>();
+//        while(queue.size()>0){
+//            result.add(queue.poll().num);
+//        }
+//        //reverse the order
+//        Collections.reverse(result);
+//
+//        return result;
+//    }
+
+    //leetcode提供思路
+       public static List<Integer> topKFrequent(int[] nums, int k) {
+            if (nums == null || nums.length == 0) return null;
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+
+            for (int i : nums) {
+                if (i < min) min = i;
+                if (i > max) max = i;
             }
+
+            int[] count = new int[max - min + 1];   // 各个数字出现的频数
+
+            for (int i : nums) {
+                count[i - min]++;
+            }
+
+            int max_count = Integer.MIN_VALUE;
+            //找出最高频率数
+            for (int i : count) {
+                if (i > max_count)
+                    max_count = i;
+            }
+
+            int[] aid = new int[max_count + 1];   // 各个频数所包含的key数
+            //比如出现过1次的三个，出现过两次的2两个
+            for (int i : count) {
+                if (i > 0) {
+                    aid[i]++;
+                }
+            }
+            //得出哪些频数需要输出即最低频数是多少
+            int min_count = 1;
+            int curr = 0;
+            for (int i = aid.length - 1; i >= 1; i--) {
+                if (aid[i] > 0) {
+                    curr += aid[i];//累积从高到低需要输出的频率
+                    if (curr >= k) {
+                        min_count = i;
+                        break;
+                    }
+                }
+            }
+            //然后从count[i]>最低频数，加入到结果集中
+            List<Integer> ans = new ArrayList<>(k);
+            for (int i = 0; i < count.length; i++) {
+                if (count[i] >= min_count) {
+                    ans.add(i + min);
+                }
+            }
+            return ans;
         }
 
-        // create a min heap
-        PriorityQueue<Pair> queue = new PriorityQueue<Pair>(new Comparator<Pair>(){
-            public int compare(Pair a, Pair b){
-                return a.count-b.count;
-            }
-        });
+    public static void main(String[] args) {
+        int []nums = {4,1,-1,2,-1,2,3};
+        List<Integer> reslist=  topKFrequent(nums,2);
 
-        //maintain a heap of size k.
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            Pair p = new Pair(entry.getKey(), entry.getValue());
-            queue.offer(p);
-            if(queue.size()>k){
-                queue.poll();
-            }
-        }
-
-        //get all elements from the heap
-        List<Integer> result = new ArrayList<Integer>();
-        while(queue.size()>0){
-            result.add(queue.poll().num);
-        }
-        //reverse the order
-        Collections.reverse(result);
-
-        return result;
     }
-//leetcode提供思路
-    //   public List<Integer> topKFrequent(int[] nums, int k) {
-    //        if (nums == null || nums.length == 0) return null;
-    //        int min = Integer.MAX_VALUE;
-    //        int max = Integer.MIN_VALUE;
-    //
-    //        for (int i : nums) {
-    //            if (i < min) min = i;
-    //            if (i > max) max = i;
-    //        }
-    //
-    //        int[] count = new int[max - min + 1];   // 各个数字出现的频数
-    //
-    //        for (int i : nums) {
-    //            count[i - min]++;
-    //        }
-    //
-    //        int max_count = Integer.MIN_VALUE;
-    //
-    //        for (int i : count) {
-    //            if (i > max_count)
-    //                max_count = i;
-    //        }
-    //
-    //        int[] aid = new int[max_count + 1];   // 各个频数所包含的key数
-    //
-    //        for (int i : count) {
-    //            if (i > 0) {
-    //                aid[i]++;
-    //            }
-    //        }
-    //
-    //        int min_count = 1;
-    //        int curr = 0;
-    //        for (int i = aid.length - 1; i >= 1; i--) {
-    //            if (aid[i] > 0) {
-    //                curr += aid[i];
-    //                if (curr >= k) {
-    //                    min_count = i;
-    //                    break;
-    //                }
-    //            }
-    //        }
-    //
-    //        List<Integer> ans = new ArrayList<>(k);
-    //        for (int i = 0; i < count.length; i++) {
-    //            if (count[i] >= min_count) {
-    //                ans.add(i + min);
-    //            }
-    //        }
-    //        return ans;
-    //    }
+
 }
