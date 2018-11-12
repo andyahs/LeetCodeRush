7e4e66c70d4c49b8b48ce0ab583c10a958748d7d
andyahs <andyhas@163.com>
2018-11-12 10:23:12 +0800

    更新300给定一个无序的整数数组，找到其中最长上升子序列的长度。

--- src/main/java/com/leetcode/_300_LongestIncreasingSubsequence.java
+++ src/main/java/com/leetcode/_300_LongestIncreasingSubsequence.java
@@ -8,9 +8,9 @@ import java.util.TreeSet;
  * 2018/10/2 22:18
  * coding For love and Peace
  *
- *300最长上升子序列
+ *`
  * Longest Increasing Subsequence
- * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ * 300给定一个无序的整数数组，找到其中最长上升子序列的长度。
  * 示例:
  * 输入: [10,9,2,5,3,7,101,18]
  * 输出: 4
@@ -24,53 +24,61 @@ import java.util.TreeSet;
  */
 public class _300_LongestIncreasingSubsequence {
     //两种算法的核心依然是构建二叉树
-    public int lengthOfLIS(int[] nums) {
-        TreeSet<Integer> set =  new TreeSet<>();
-        for (int num :
-                nums) {
-            Integer ceil = set.ceiling(num);
-            if (ceil != null) set.remove(ceil);
-            set.add(num);
-        }
-        return set.size();
-     }
+//    public int lengthOfLIS(int[] nums) {
+//        TreeSet<Integer> set =  new TreeSet<>();
+//        for (int num :
+//                nums) {
+//            Integer ceil = set.ceiling(num);
+//            if (ceil != null) set.remove(ceil);
+//            set.add(num);
+//        }
+//        return set.size();
+//     }
 
      //LeetCode提供
-    //    public int lengthOfLIS(int[] nums) {
-    //        int n = nums.length;
-    //        if(n==0){
-    //            return 0;
-    //        }
-    //        if(n==1){
-    //            return 1;
-    //        }
-    //        int[] dp = new int[n];
-    //        int[] end = new int[n+1];
-    //        end[1] = nums[0];
-    //        int len = 1;
-    //        for(int i=1;i<n;i++) {
-    //            int ps = binary(end,1,len,nums[i]);
-    //            end[ps] = nums[i];
-    //            if(len < ps){
-    //                len = ps;
-    //            }
-    //        }
-    //        return len;
-    //    }
-    //
-    //    public int binary(int[] arr,int s,int e,int val) {
-    //        int mid;
-    //        if(arr[e] < val) {
-    //            return e+1;
-    //        }
-    //        while(s<e) {
-    //            mid = s + (e-s) /2;
-    //            if(arr[mid] < val) {
-    //                s = mid + 1;
-    //            } else {
-    //                e = mid;
-    //            }
-    //        }
-    //        return s;
-    //    }
+        public static int lengthOfLIS(int[] nums) {
+            int n = nums.length;
+            if(n==0){
+                return 0;
+            }
+            if(n==1){
+                return 1;
+            }
+            int[] end = new int[n+1];
+            end[1] = nums[0];
+            int len = 1;
+            for(int i=1;i<n;i++) {
+                int ps = binary(end,1,len,nums[i]);
+                //替换掉这个位置的数，因为替换后长度不变，后面的也可以继续替换，直至
+                //长度超过原来的长度,相当于把二叉树合并成一条，但是唯一有效数据是长度
+                end[ps] = nums[i];
+                if(len < ps){
+                    len = ps;
+                }
+            }
+            return len;
+        }
+
+        public static int binary(int[] end,int s,int e,int val) {
+            int mid;
+            //大于当前长度记录的最大值则直接加一
+            if(end[e] < val) {
+                return e+1;
+            }
+            //返回应当插入的位置
+            while(s<e) {
+                mid = s + (e-s) /2;
+                if(end[mid] < val) {
+                    s = mid + 1;
+                } else {
+                    e = mid;
+                }
+            }
+            return s;
+        }
+
+    public static void main(String[] args) {
+        int[] nums =  {1,3,2};
+        System.out.println(lengthOfLIS(nums));
+    }
 }
